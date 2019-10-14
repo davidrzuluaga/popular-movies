8123dde99d58d90b80f641a4c48b381c0176f660
davidrzuluaga <david@davidrzuluaga.com>
2019-10-14 17:57:58 -0500

    Updates

---
+++ precache-manifest.880d2581550ce7f06f9e855614ea44d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9aa4ed741e18c98d505d5bba6fa29a",
+    "url": "/popular-movies/index.html"
+  },
+  {
+    "revision": "babb956c272a92e7e603",
+    "url": "/popular-movies/static/css/main.495765b1.chunk.css"
+  },
+  {
+    "revision": "9fda98fc48b447254da7",
+    "url": "/popular-movies/static/js/2.e7a351ee.chunk.js"
+  },
+  {
+    "revision": "babb956c272a92e7e603",
+    "url": "/popular-movies/static/js/main.3ddf5e27.chunk.js"
+  },
+  {
+    "revision": "dfd21e45fb0572b0fd8a",
+    "url": "/popular-movies/static/js/runtime-main.89327c1f.js"
+  }
+]);
