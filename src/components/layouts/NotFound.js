5f350bf40238a14e09ac739284db103865a87bb2
davidrzuluaga <david@davidrzuluaga.com>
2019-10-14 18:12:14 -0500

    react-router-dom redirect on 404

--- src/components/layouts/NotFound.js
+++ src/components/layouts/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 const NotFound = () => {
   return (
     <div className="notfound">
       <p>Not Found</p>
+      <Redirect to="/" />
     </div>
   );
 };
