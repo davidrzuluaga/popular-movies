3e9f5dc672893f76c9f95e9534a7f559007b3cb2
davidrzuluaga <david@davidrzuluaga.com>
2019-10-14 18:28:38 -0500

    get more button

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -6,19 +6,24 @@ import ListOfMovies from '../layouts/listOfMovies';
 const Welcome = props => {
   const [theTop20Movies, setTheTop20Movies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setcurrentPage] = useState(1);
 
   useEffect(() => {
     getTheTop20Movies();
   }, []);
 
+  useEffect(() => {
+    getTheTop20Movies();
+  }, [currentPage]);
+
   function getTheTop20Movies() {
     axios({
       method: 'GET',
-      url: `https://api.themoviedb.org/3/movie/popular?api_key=ee41860b27e6bb1be4f182a4519564aa&language=en-US&page=1`
+      url: `https://api.themoviedb.org/3/movie/popular?api_key=ee41860b27e6bb1be4f182a4519564aa&language=en-US&page=${currentPage}`
     })
       .then(response => {
         setLoading(false);
-        setTheTop20Movies(response.data.results);
+        setTheTop20Movies([...theTop20Movies, ...response.data.results]);
       })
       .catch(e => {
         setLoading(false);
@@ -26,11 +31,25 @@ const Welcome = props => {
   }
 
   return (
-    <ListOfMovies
-      movies={theTop20Movies}
-      loading={loading}
-      title={'Top Movies'}
-    />
+    <>
+      <ListOfMovies
+        movies={theTop20Movies}
+        loading={loading}
+        title={'Top Movies'}
+      />
+      <button
+        onClick={() => setcurrentPage(currentPage + 1)}
+        style={{
+          width: '100%',
+          backgroundColor: 'gray',
+          margin: '0 0 10px 0',
+          height: '80px',
+          fontSize: '50px'
+        }}
+      >
+        Get More
+      </button>
+    </>
   );
 };
 
