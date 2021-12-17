f1154c2473bf55dc2b1cc74c7c098820a6e863a6
David Zuluaga <david@davidrzuluaga.com>
2021-12-17 09:04:14 -0500

    node strict

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+if (window.location.hostname !== 'localhost') {
+  Sentry.init({
+    dsn: "https://b8108804d3114a30ab53c7aaa0f5d04b@o482482.ingest.sentry.io/6111840",
+    integrations: [new Integrations.BrowserTracing()],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
 ReactDOM.render(
   <Router>
     <App />
